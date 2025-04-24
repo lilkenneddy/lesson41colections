d3a7b1e76673ec288c0a12f3c7bade50ebbb2abe
Rostyslav Skliaruk <sklyaruk.rostislav1903@gmail.com>
2025-04-24 17:07:17 +0200

    completed homework

---
+++ src/main/java/org/example/Car.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import static java.lang.Long.compare;
+
+public class Car implements Comparable<Car> {
+    private int yearOfManufacture;
+    private int engineCapacity;
+
+    public Car(int yearOfManufacture) {
+    }
+
+    public Car(int yearOfManufacture, int engineCapacity) {
+        this.yearOfManufacture = yearOfManufacture;
+        this.engineCapacity = engineCapacity;
+    }
+
+    public int getYearOfManufacture() {
+        return yearOfManufacture;
+    }
+
+    public void setYearOfManufacture(int yearOfManufacture) {
+        this.yearOfManufacture = yearOfManufacture;
+    }
+
+    public int getEngineCapacity() {
+        return engineCapacity;
+    }
+
+    public void setEngineCapacity(int engineCapacity) {
+        this.engineCapacity = engineCapacity;
+    }
+
+    @Override
+    public int compareTo(Car o) {
+
+        return Integer.compare(getYearOfManufacture(), getEngineCapacity());
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,24 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        List<Car> cars = new ArrayList<>();
+        Car car1 = new Car(2024);
+        Car car2 = new Car(2018);
+        Car car3 = new Car(2025);
+        cars.add(car1);
+        cars.add(car2);
+        cars.add(car3);
+        System.out.println("before sorting : " + cars);
+        Collections.sort(cars);
+        System.out.println("after sorting : " + cars);
     }
+
 }
